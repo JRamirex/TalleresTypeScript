94ec7a49bca542ca06a3c5ad1a51353d3ebcb1d8
jramirezbUniandes <69643776+jramirezbUniandes@users.noreply.github.com>
2021-04-14 17:49:11 -0500

    Archivos taller 1 agregados

---
+++ tallerTypescript/dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "Jose Bocanegra", 3),
+  new Course("Diseño de alogrítmos", "Rodrigo Cardoso", 3),
+  new Course("TI en las organizaciones", "Oscar Gonzalez", 3),
+  new Course("Sistemas transaccionales", "German Bravo", 3),
+  new Course("Lenguajes y maquinas", "Silvia Takahashi", 3)
+]
